6db66ab4cdad6d167a3642b7bde887d9b05349b0
Евгений Орел <eorel@sslntwrk.lan>
2019-11-28 11:22:37 +0300

    Link creation

--- utils/http_build_graph.py
+++ utils/http_build_graph.py
@@ -1,6 +1,16 @@
 import requests
 import json
 
+'''
+Make link:
+https://3vis.neobit.ru/api/graph/link
+
+{"sourceId":7710,"targetId":7690,"pid":"503868d2-97f8-4274-b0f5-46034000803f"}
+
+Make node:
+need to get id:
+response['msg']['node']['id']
+'''
 class GraphCreationError(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
@@ -10,6 +20,8 @@ class GraphVisualizer:
     def __init__(self, graph_name):
         self.name = graph_name
         self.graph_id = None
+		self.graph_links = []
+		self.nodes_map = {}
         self.auth_token = ("BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzcyOTk5ODgsImlhdCI6MTU3NDcwNzk4OCw"
                            "iaWQiOjUsInN1YiI6Im9ybHlfdGVzdGVyIn0.MI8itHOSF81GOKwjMgfXTSCztlceTd6reJHO33VeqP8")
     def create_graph(self):
@@ -39,19 +51,35 @@ class GraphVisualizer:
         node_params = {
                         "node":
                            {
-                               "x":1,"y":1,"name":node_name,"type":node_type
+                               "name":node_name,"type":node_type
                            },
                        "pid": self.graph_id,
-                       "needCompare":True
+                       "needCompare":False
         }
         r = requests.post("https://3vis.neobit.ru/api/graph/node", json=node_params,
                           headers={"Authorization":self.auth_token})
 
         if r.status_code != 200:
             raise GraphCreationError("Node '{node_name}' type '{node_type}' was not created")
+		
+		try:
+            response = json.loads(r.text)
+        except Exception as ex:
+            raise GraphCreationError(f"Graph node creation response error: {ex}")
+			
+		self.nodes_map[node_name] = response['msg']['node']['id']
 
-    def add_edge(self, node_first, node_second):
-        pass
+    def add_edge(self, first_node_id, second_node_id, cve):
+	    link_params = {"sourceId":first_node_id,"targetId":second_node_id,"pid":self.graph_id}
+		r = requests.post('https://3vis.neobit.ru/api/graph/link', json=link_params,
+						   headers={"Authorization":self.auth_token})
+        
+		try:
+            response = json.loads(r.text)
+        except Exception as ex:
+            raise GraphCreationError(f"Graph node creation response error: {ex}")
+		
+		self.graph_links.append((first_node_id,second_node_id,cve))
 if __name__ == '__main__':
     vizual = GraphVisualizer("test_graph")
     vizual.create_graph()
